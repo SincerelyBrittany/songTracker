fae904e89f100121c8bd8dccb85162cd801fd6b0
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-03 16:04:06 -0400

    set up api manager for soundcloud in helpers

---
+++ app/helpers/api_manager.rb
@@ -0,0 +1,17 @@
+# require './config/environment'
+# require 'soundcloud'
+# require 'pry'
+#
+# class APImanager
+# # create a client object with your app credentials
+# CLIENT = Soundcloud.new(:client_id => "#{CLIENT_ID}")
+# # find all sounds of buskers licensed under 'creative commons share alike'
+# def self.get_tracks(query)
+# tracks = CLIENT.get('/tracks', :q => "#{query}", :licence => 'cc-by-sa')
+# end
+# #title
+# #artwork_url
+# #permalink_url
+# binding.pry
+#
+# end

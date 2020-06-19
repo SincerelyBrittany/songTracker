22e0747cbbe5cd8d2b00cb0a140348a2f851d8ad
SincerelyBrittany <sincerelybr@gmail.com>
2020-06-19 18:20:35 -0400

    merge

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -27,48 +27,6 @@ class PlaylistsController < ApplicationController
         end
    end
 
-#    post '/wines' do
-#     country = Country.find_by(name: params[:country])
-#     @wine = Wine.new(
-#     name: sanitize(params[:name]),
-#     country: country,
-#     wine_type: sanitize(params[:wine_type]),
-#     price: params[:price],
-#     year_sealed: params[:year]
-#   )
-#   if @wine.save
-#     current_user.wines << @wine
-#     redirect "users/#{current_user.slug}"
-#   else
-#     @countries = Country.all.order(:name)
-#     erb :'wines/new'
-#   end
-#
-# end
-
-#
-#    post '/watchlists' do
-#   authenticate
-#  if params[:watchlist][:stock] == nil
-#    @watchlist = current_user.watchlists.new(name: Sanitize.fragment(params[:watchlist][:name]), user_id: params[:user_id])
-#    # binding.pry
-#    if @watchlist.save
-#    redirect "/watchlists"
-#    else
-#      redirect "/watchlists/new"
-#    end
-# else
-#   @stock = Stock.create(name: params[:watchlist][:stock][:name], price: params[:watchlist][:stock][:price],
-#   logo: params[:watchlist][:stock][:logo], ceo: params[:watchlist][:stock][:ceo],
-#   description: params[:watchlist][:stock][:description], ticker: params[:watchlist][:stock][:ticker])
-#   @watchlist = Watchlist.find(params[:watchlist][:id][0])
-#   @watchlist.stocks << @stock
-#
-#   redirect "/watchlists/#{@watchlist.id}"
-# end
-# end
-
-
 
    get '/playlists/:id/edit' do
      # binding.pry

1751526ca270300fb21752b0b68ab5e09418497e
SincerelyBrittany <sincerelybr@gmail.com>
2020-06-19 18:08:28 -0400

    updated the users controller to find_by id instead of slug

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -6,6 +6,7 @@ class PlaylistsController < ApplicationController
   get '/playlists' do #works
      authenticate
      @playlists = current_user.playlists
+      # binding.pry
      erb :'playlists/index'
    end
 
@@ -16,15 +17,61 @@ class PlaylistsController < ApplicationController
 
    post '/playlists' do
      authenticate
-     # binding.pry
-     Playlist.create(name: params[:playlist][:name], user_id: current_user.id)
-     flash[:message] = "Successfully created song."
-     # redirect("/songs/#{@song.slug}")
-     redirect '/playlists'
+     binding.pry
+     @playlist = Playlist.new(name: params[:playlist][:name], user_id: current_user.id)
 
+        if @playlist.save
+          redirect "/playlists"
+        else
+           redirect "/playlists/new"
+        end
    end
 
-   get '/playlists/:slug/edit' do
+#    post '/wines' do
+#     country = Country.find_by(name: params[:country])
+#     @wine = Wine.new(
+#     name: sanitize(params[:name]),
+#     country: country,
+#     wine_type: sanitize(params[:wine_type]),
+#     price: params[:price],
+#     year_sealed: params[:year]
+#   )
+#   if @wine.save
+#     current_user.wines << @wine
+#     redirect "users/#{current_user.slug}"
+#   else
+#     @countries = Country.all.order(:name)
+#     erb :'wines/new'
+#   end
+#
+# end
+
+#
+#    post '/watchlists' do
+#   authenticate
+#  if params[:watchlist][:stock] == nil
+#    @watchlist = current_user.watchlists.new(name: Sanitize.fragment(params[:watchlist][:name]), user_id: params[:user_id])
+#    # binding.pry
+#    if @watchlist.save
+#    redirect "/watchlists"
+#    else
+#      redirect "/watchlists/new"
+#    end
+# else
+#   @stock = Stock.create(name: params[:watchlist][:stock][:name], price: params[:watchlist][:stock][:price],
+#   logo: params[:watchlist][:stock][:logo], ceo: params[:watchlist][:stock][:ceo],
+#   description: params[:watchlist][:stock][:description], ticker: params[:watchlist][:stock][:ticker])
+#   @watchlist = Watchlist.find(params[:watchlist][:id][0])
+#   @watchlist.stocks << @stock
+#
+#   redirect "/watchlists/#{@watchlist.id}"
+# end
+# end
+
+
+
+   get '/playlists/:id/edit' do
+     # binding.pry
      @playlist = Playlist.find_by(id: params[:id])
      authorize(@playlist)
      # binding.pry
@@ -35,7 +82,7 @@ class PlaylistsController < ApplicationController
      @playlist = Playlist.find_by(id: params[:id])
      authorize(@playlist)
      if @playlist.update(name: params[:playlist][:name])
-       redirect '/playlists'
+       redirect "users/#{current_user.id}"
      else
        redirect "/playlists/#{@playlist.id}/edit"
      end
@@ -44,6 +91,7 @@ class PlaylistsController < ApplicationController
    get '/playlists/:id' do
      authenticate
      @playlist = Playlist.find_by(id: params[:id])
+      # authorize(@playlist.user)
      erb :'/playlists/show'
    end
 
@@ -51,7 +99,7 @@ class PlaylistsController < ApplicationController
      @playlist = Playlist.find_by(id: params[:id])
      authorize(@playlist)
      @playlist.destroy
-     redirect '/playlists'
+     redirect "users/#{current_user.id}"
    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
-#MUST WORK ON
 class UsersController < ApplicationController
-  get '/users/:slug' do
-    user = User.find_by_slug(params[:slug])
-    @playlists = user.playlists
+
+  get '/users/:id' do
+    @user = User.find_by(params[:id])
+    authorize_user(@user)
     erb :'users/show'
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find_by(params[:id])
+    authorize_user(@user)
+    erb :'users/edit'
+  end
+
+  patch '/users/:id' do
+    @user = User.find_by(params[:id])
+    authorize_user(@user)
+    if @user.update(username: sanitize(params[:username]), email: params[:email], password: params[:password])
+      redirect "users/#{@user.id}"
+    else
+      erb :"users/<%=@user.id%>/edit"
+    end
   end
 
 end
